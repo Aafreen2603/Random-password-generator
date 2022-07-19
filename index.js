13372656bfa44cf455b0d3108283aa2a9f5303f8
Aafreen2603 <96076983+Aafreen2603@users.noreply.github.com>
2022-07-19 15:32:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const characters=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+let n = characters.length
+let passEl = document.getElementById("pass")
+let password = " "
+
+function generate(){
+    for(let i=0; i<10; i++){
+        let randIndex = Math.floor(Math.random()*n)
+        password += characters[randIndex]
+    }
+    passEl.textContent += password
+}
+
